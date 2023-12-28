a787af2a6bdd7115a45830a135e80d1652708bb2
feder-cr <federico.elia.majo@gmail.com>
2023-12-28 17:10:07 +0000

    modificato il decode_buffer per creare il giusto output(rimane ancora qualcosa da completare)

--- lavoroFedericoElia/ws/decode_buffer.js
+++ lavoroFedericoElia/ws/decode_buffer.js
@@ -5,61 +5,81 @@ const { WebSocket } = require("ws")
 const mqtt = require('mqtt-packet');
 const { error } = require("console");
 const opts = { protocolVersion: 4 }; // default is 4. Usually, opts is a connect packet
-// const parser = mqtt.parser(opts);
+const parser = mqtt.parser(opts);
+let TCPMessage
 
 const byLine = readline.createInterface(stdin)
 const ws = new WebSocket("ws://localhost:8080/")
 // const ws = new WebSocket("ws://druidlab.dibris.unige.it:8080")
 
-// (pid,fd) -> mqtt-parser
-const parserMap = new Map()
 
 
+function RGBInRGB565(inputJSON) {
+    // Estrai il valore dalla chiave "value" nell'oggetto JSON
+	
+	try{
+		//console.log(JSON.parse(inputJSON).value)
+	//	console.log(inputJSON)
+		const stringa = JSON.parse(inputJSON).value;
+		//console.log("-----------------"+ JSON.parse(inputJSON).value + "-----------------")
+		// Estrai i valori R, G e B dalla stringa
+		const match = stringa.match(/(\d+)-(\d+)-(\d+)/);
+
+		const r = parseInt(match[1]);
+		const g = parseInt(match[2]);
+		const b = parseInt(match[3]);
+
+		// Converti i valori RGB nel formato RGB565
+		const rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
+
+		return hexToRGB565(rgb565.toString(16));
+	}catch (error) {
+		console.error("Error in RGBInRGB565 function:", error);
+		// Handle the error as needed
+		// You might want to return a default value or throw the error again
+		return "null";
+	}
+}
+
+
+function hexToRGB565(hexValue) {
+    const intValue = parseInt(hexValue, 16);
+
+    let binaryString = intValue.toString(2);
+
+	while (binaryString.length < 16) {
+		binaryString = '0' + binaryString;
+	}
+
+    const binaryRed = binaryString.substring(0, 5);
+    const intRed = parseInt(binaryRed, 2);
+
+	const binaryGreen = binaryString.substring(5, 11);
+    const intGreen = parseInt(binaryGreen, 2);
+
+	const binaryBlue = binaryString.substring(11, 16);
+    const intBlue = parseInt(binaryBlue, 2);
+
+    return {r: intRed,g: intGreen,b: intBlue};
+}
 
 
 
 // decode base64 encoded buffer and parse mqtt packet
 function decode_base64(json) {
 	if (json.rule === "tcp_syscalls") { // rule for mqtt
-		if ("evt.buffer" in json.output_fields && json.output_fields['evt.buffer'] != null) {
-			const mapKey = json.output_fields["proc.pid"] << 32 + json.output_fields["fd.num"]
-	
-			let parser = parserMap.get(mapKey)
-			if (parser === undefined) {
-				console.error("creating new parser for ",
-					json.output_fields["proc.pid"], json.output_fields["fd.num"])
-				parser = mqtt.parser(opts)
-				// TODO: closing of the file descriptor should be handled in order 
-				// to avoid memory leaking and concatenating data from different connection
-				parser.on('error', err => {
-					console.error("failing to parse: ", err.message, json.output_fields["proc.pid"], json.output_fields["fd.num"])
-				})
-	
-				parserMap.set(mapKey, parser)
-			}
-	
-			let packets = []
-			parser.on('packet', packet => {
-				packets.push(packet)
-			})
-	
-	
-			parser.parse(Buffer.from(json.output_fields['evt.buffer'], 'base64'))
-	
-			parser.removeAllListeners('packet')
-	
-			// instead of sending an mqtt packet we send an array of packets
-			// because multiple packets could be read in a single syscall.
-			// a possible better approach would be to send data each time a complete mqtt
-			// packet is parsed, possibly with some additional information got from the syscall
-			// (for example timestamp of the last syscall)
-			json.output_fields['evt.buffer'] = packets
+		//json.output_fields['evt.buffer'] = Buffer.from(json.output_fields['evt.buffer'], 'base64').toString();
+
+
+		if ("evt.buffer" in json.output_fields && json.output_fields['evt.buffer'] != null)		  
+		{
+			parser.parse(atob(json.output_fields['evt.buffer']))
 		}
+        json.output_fields['evt.buffer'] = TCPMessage;			
 	} else if(json.rule === "sense-hat") {
 		const hexBuffer = Buffer.from(json.output_fields['evt.buffer'], 'base64').toString('hex');
-
-        // Aggiornamento dell'output con il buffer convertito
-        json.output_fields['evt.buffer'] = hexBuffer;
+		const decimalValue = hexBuffer.substring(2, 6);
+        json.output_fields['evt.buffer'] = hexToRGB565(decimalValue);
     }
 }
 
@@ -100,6 +120,7 @@ function send_falco_event(ws, json) {
 ws.on("open", () => {
 	console.log("ws connection open")
 	byLine.on("line", line => {
+		//console.log(line)
 		const json = JSON.parse(line)
 		send_falco_event(ws, json)
 	})
@@ -107,15 +128,25 @@ ws.on("open", () => {
 
 
 ws.on("message", msg => {
-	const json = JSON.parse(msg)
-	console.log(JSON.stringify(json))
+	//const json = JSON.parse(msg)
+	//console.log(JSON.stringify(json))
 })
 
 ws.on("error", error => {
-	console.error(`[decode_buffer.js] ${error.msg}`)
+	//console.error(`[decode_buffer.js] ${error.msg}`)
 })
 
 ws.on("close", () => {
 	process.exit()
 })
+
+parser.on('packet', packet => {
+	packet.payload=RGBInRGB565(packet.payload)
+	TCPMessage = packet
+  })
+
+
+
+
+
 
--- lavoroFedericoElia/ws/ws_server.js
+++ lavoroFedericoElia/ws/ws_server.js
@@ -23,26 +23,6 @@ const logger_sense_hat = winston.createLogger({
   format: winston.format.json()
 });
 
-function convertiStringaInRGB565(inputJSON) {
-    // Estrai il valore dalla chiave "value" nell'oggetto JSON
-    const stringa = JSON.parse(inputJSON).value;
-
-    // Estrai i valori R, G e B dalla stringa
-    const match = stringa.match(/(\d+)-(\d+)-(\d+)/);
-
-    if (!match) {
-        throw new Error("Formato stringa non valido. Assicurati che sia nel formato 'R-G-B'");
-    }
-
-    const r = parseInt(match[1]);
-    const g = parseInt(match[2]);
-    const b = parseInt(match[3]);
-
-    // Converti i valori RGB nel formato RGB565
-    const rgb565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
-
-    return rgb565;
-}
 
 server.on('connection', function(socket) {
   socket.on('message', function(msg) {
@@ -50,24 +30,12 @@ const parsedMsg = JSON.parse(msg.toString());
     if (
       parsedMsg.rule === "tcp_syscalls" &&
       parsedMsg.output_fields &&
-      parsedMsg.output_fields["evt.buffer"] &&
-      parsedMsg.output_fields["evt.buffer"][0] &&
-      parsedMsg.output_fields["evt.buffer"][0].payload &&
-      parsedMsg.output_fields["evt.buffer"][0].payload.data !== null
-    ){
-                const payloadArray = parsedMsg.output_fields["evt.buffer"][0].payload.data;
-                // Converti l'array in una stringa
-                const payloadString = String.fromCharCode(...payloadArray);
-                // Verifica il formato del payload
-                const regex = /^\{"value": "\d+-\d+-\d+"\}$/;
-                if (regex.test(payloadString)) {
-                   parsedMsg.output_fields["evt.buffer.format"]= "Correct";
-                   parsedMsg.output_fields["evt.buffer.rgb565"]= convertiStringaInRGB565(payloadString).toString(16);
-                }else {
-                   parsedMsg.output_fields["evt.format"]= "Wrong"
-                }
-                logger_tcp_syscalls.info(parsedMsg);                            
-  }else if(parsedMsg.rule === "sense-hat" && parsedMsg.output_fields["evt.buffer"] != null)
+      parsedMsg.output_fields["evt.buffer"]
+    )
+    {
+        logger_tcp_syscalls.info(parsedMsg);                            
+    }
+  else if(parsedMsg.rule === "sense-hat" && parsedMsg.output_fields["evt.buffer"] != null)
   {
     logger_sense_hat.info(parsedMsg);
   }else{
