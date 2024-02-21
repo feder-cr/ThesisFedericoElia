f33137b62e10a7f2c9e5edadc6ebcb3624b971d7
davideancona <davide.ancona@unige.it>
2024-02-21 17:56:01 +0100

    specifications new file:   lavoroFedericoElia/spec/.gitignore new file:   lavoroFedericoElia/spec/mqttSimple.pl new file:   lavoroFedericoElia/spec/mqttSimple.rml

---
+++ lavoroFedericoElia/spec/mqttSimple.pl
@@ -0,0 +1,7 @@
+:- module(spec, [(trace_expression/2), (match/2)]).
+:- use_module(monitor(deep_subdict)).
+:- use_module(library(clpr)).
+match(_event, displayColor_et(R, G, B)) :- deep_subdict(_event, _{event:"mqtt", msg:_{output_fields:_{'evt.buffer':_{cmd:"publish", topic:"displayColor", payload:_{red:R, blue:B, green:G}}, 'evt.type':"read"}}}).
+match(_event, any_et) :- deep_subdict(_event, _{}).
+match(_event, none_et) :- not(match(_event, any_et)).
+trace_expression('Main', Main) :- (Main=var(r, var(g, var(b, displayColor_et(var(r), var(g), var(b)))))).
