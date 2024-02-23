2dcd96a98cddf7c58de2f8c5da45410e960c0bf6
Federico <85809106+feder-cr@users.noreply.github.com>
2024-02-23 19:35:21 +0100

    Add files via upload

--- lavoroFedericoElia/clientPub.js
+++ lavoroFedericoElia/clientPub.js
@@ -15,12 +15,57 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const possibleValues = [
+    '10',                    // Stringa numerica
+    '100',                   // Stringa numerica
+    '"tringa"',
+    "stringa",               // Stringa senza apici extra
+    JSON.stringify({ color: 'colore' }),  // Stringa JSON rappresentante un oggetto
+    JSON.stringify({ 
+      red: 100.1, 
+      blue: 100.1, 
+      green: 100.1 
+    }),  // Stringa JSON rappresentante un oggetto
+    'true',                  // Stringa booleana
+    'null',                  // Stringa rappresentante il valore null
+    JSON.stringify([1, 2, 3, 4]),  // Stringa JSON rappresentante un array
+  ];
+var value = 0
 function pub() {
-    var randomValue = getRandomNumber(0, 255);
+    var randomValue1 = getRandomNumber(0, 255);
     var randomValue2 = getRandomNumber(0, 255);
     var randomValue3 = getRandomNumber(0, 255);
-    client.publish(`${topic}/two`, `{"value": "${randomValue}-${randomValue2}-${randomValue3}"}`);
+    var jsonObject = {
+        red: randomValue1,
+        green: randomValue2,
+        blue: randomValue3
+    };
+    //client.publish(`displayColor`, JSON.stringify(jsonObject));
+    
+
+    var randomNumber = Math.random();
+    // Utilizza un'istruzione if per decidere se è vero o falso
+    var isTrue = randomNumber > 0.5;
+
+    // Stampa il risultato
+    if (isTrue) {
+        client.publish(`displayColor`, JSON.stringify(jsonObject));
+    } else {
+        client.publish('displayColor', possibleValues[value]);      
+    }
+
+
+
+    // pubblica possibleValues ogni 4 messaggi
+    //const value = possibleValues[Math.floor(Math.random() * possibleValues.length)];
+   // client.publish('displayColor', value);
+    //client.publish('displayColor', 'ciao');
+    
+    value++;
+    if (value==possibleValues.length) {
+        value=0;
+    }
 }
 
-setInterval(pub,5000);
+setInterval(pub,1000);
 
