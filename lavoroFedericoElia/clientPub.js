d8fe68f23f68f73d5859f8a55a695174f7a39a2b
feder-cr <federico.elia.majo@gmail.com>
2024-02-29 23:57:13 +0000

    linting uniforme al resto del codice

--- lavoroFedericoElia/clientPub.js
+++ lavoroFedericoElia/clientPub.js
@@ -1,71 +1,70 @@
-'use strict';
-
 const { connect } = require('mqtt');
 
 const topic = 'displayColor';
 const settings = {
-    port: 1883
+    port: 1883,
 };
 
 const client = connect('mqtt://broker.hivemq.com', settings);
 client.subscribe(`${topic}/+`);
 
-
-function getRandomNumber(min, max) {
+function getRandomNumber(min, max)
+{
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const possibleValues = [
-    '10',                    // Stringa numerica
-    '100',                   // Stringa numerica
+    '10', // Stringa numerica
+    '100', // Stringa numerica
     '"tringa"',
-    "stringa",               // Stringa senza apici extra
-    JSON.stringify({ color: 'colore' }),  // Stringa JSON rappresentante un oggetto
-    JSON.stringify({ 
-      red: 100.1, 
-      blue: 100.1, 
-      green: 100.1 
-    }),  // Stringa JSON rappresentante un oggetto
-    'true',                  // Stringa booleana
-    'null',                  // Stringa rappresentante il valore null
-    JSON.stringify([1, 2, 3, 4]),  // Stringa JSON rappresentante un array
-  ];
-var value = 0
-function pub() {
-    var randomValue1 = getRandomNumber(0, 255);
-    var randomValue2 = getRandomNumber(0, 255);
-    var randomValue3 = getRandomNumber(0, 255);
-    var jsonObject = {
+    'stringa', // Stringa senza apici extra
+    JSON.stringify({ color: 'colore' }), // Stringa JSON rappresentante un oggetto
+    JSON.stringify({
+        red: 100.1,
+        blue: 100.1,
+        green: 100.1,
+    }), // Stringa JSON rappresentante un oggetto
+    'true', // Stringa booleana
+    'null', // Stringa rappresentante il valore null
+    JSON.stringify([1, 2, 3, 4]), // Stringa JSON rappresentante un array
+];
+let value = 0;
+function pub()
+{
+    const randomValue1 = getRandomNumber(0, 255);
+    const randomValue2 = getRandomNumber(0, 255);
+    const randomValue3 = getRandomNumber(0, 255);
+    const jsonObject = {
         red: randomValue1,
         green: randomValue2,
-        blue: randomValue3
+        blue: randomValue3,
     };
-    //client.publish(`displayColor`, JSON.stringify(jsonObject));
-    
+    // client.publish(`displayColor`, JSON.stringify(jsonObject));
 
-    var randomNumber = Math.random();
+    const randomNumber = Math.random();
     // Utilizza un'istruzione if per decidere se è vero o falso
-    var isTrue = randomNumber > 0.5;
+    const isTrue = randomNumber > 0.5;
 
     // Stampa il risultato
-    if (isTrue) {
-        client.publish(`displayColor`, JSON.stringify(jsonObject));
-    } else {
-        client.publish('displayColor', possibleValues[value]);      
+    if (isTrue)
+    {
+        client.publish('displayColor', JSON.stringify(jsonObject));
+    }
+    else
+    {
+        client.publish('displayColor', possibleValues[value]);
     }
 
-
-
     // pubblica possibleValues ogni 4 messaggi
-    //const value = possibleValues[Math.floor(Math.random() * possibleValues.length)];
-   // client.publish('displayColor', value);
-    //client.publish('displayColor', 'ciao');
-    
-    value++;
-    if (value==possibleValues.length) {
-        value=0;
+    // const value = possibleValues[Math.floor(Math.random() * possibleValues.length)];
+    // client.publish('displayColor', value);
+    // client.publish('displayColor', 'ciao');
+
+    value += 1;
+    if (value === possibleValues.length)
+    {
+        value = 0;
     }
 }
 
-setInterval(pub,1000);
-
+setInterval(pub, 1000);
