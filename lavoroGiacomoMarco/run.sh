0d27d8fc36836a99a13a179f6063914f53a2b427
Federico <85809106+feder-cr@users.noreply.github.com>
2024-06-05 14:29:34 +0100

    Delete lavoroGiacomoMarco directory

--- lavoroGiacomoMarco/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#start falco with log_server configurations and pipe stout in decode_buffer
-sudo falco -c falco/falco.yaml -r falco/log_server.yaml -r falco/sense-hat.yaml -b -A \
-	| node ws/decode_buffer.js
